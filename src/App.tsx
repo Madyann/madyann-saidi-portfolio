443b4f6ea15414f5afea481600b4da7985370770
Madyann Saidi <madyannsaidi@gmail.com>
2025-09-01 13:15:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/madyann-saidi-portfolio/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/projects" element={<Projects />} />
