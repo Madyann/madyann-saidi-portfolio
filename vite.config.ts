b8f9a102cb360d1ecdc80424c5baab6f328af7a0
Madyann Saidi <madyannsaidi@gmail.com>
2025-09-22 17:22:36 -0400

    Attemping a fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-   server: {
-    host: "::",
-    port: 8080,
-  },
-  // Add this line right here
-  base: 'madyann-saidi-portfolio/',
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//    server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   // Add this line right here
+//   base: 'madyann-saidi-portfolio/',
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  base: '/madyann-saidi-portfolio/',
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
+});
